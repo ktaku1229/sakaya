17a08c3585ff10ff9d90a4b09e49e883a2df3a2e
ktaku1229 <ktaku1229@gmail.com>
2017-07-21 16:08:50 +0900

    add entities

---
+++ src/Drink.java
@@ -0,0 +1,20 @@
+
+public class Drink {
+
+	private String brand;
+	private int num;
+
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+}
---
+++ src/Order.java
@@ -0,0 +1,26 @@
+
+public class Order {
+
+	private int number;
+	private Customer customer;
+	private Drink drink;
+
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public Drink getDrink() {
+		return drink;
+	}
+	public void setDrink(Drink drink) {
+		this.drink = drink;
+	}
+}
