8acc4187a664d89775bc31c3f4a470c37fd30bd4
左岸清河 <zuoanqh@users.noreply.github.com>
2017-07-27 11:35:26 +0900

    Update DisplayLists.java

--- src/DisplayLists.java
+++ src/DisplayLists.java
@@ -8,40 +8,30 @@ import javafx.stage.Stage;
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DisplayLists {
-
-    public static void main(String[] args) {
-        List<Stock> l = new ArrayList<Stock>();
-        Stock s = new Stock();
-        Drink d = new Drink();
-        d.setBrand("asdasda");
-        d.setNum(123123);
-        s. setStock(d);
-        l.add(s);
-        Ticket t = new Ticket();
-        t.setDrink(d);
-        t.setNumber(144);
-        Customer c = new Customer();
-        c.setName("cat ajsdoas");
-        t.setCustomer(c);
-        TicketList tl = new TicketList();
-        tl.ticket.add(t);
-        DisplayTicketList(tl);
-
+    static{
+        FileTest.main(null);
     }
 
-    public static void DisplayStock(List<Stock> l)
+    public static void main(String[] args) {
+        DisplayStock();
+    }
+
+    public static void DisplayStock()
     {
+        JOptionPane.showMessageDialog(null, "");
+        Stock l = FileTest.s;
         JFrame frame = new JFrame("JTable Test");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         String[] columnNames = {"Brand", "Num"};
 
-        Object[][] data = new String[l.size()][2];
-        for(int i=0;i<l.size();i++)
+        Object[][] data = new String[l.getStock().size()][2];
+        for(int i=0;i<l.getStock().size();i++)
         {
-            data[i][0]=l.get(i).getStock().getBrand();
-            data[i][1]=l.get(i).getStock().getNum()+"";
+            data[i][0]=l.getStock().get(i).getBrand();
+            data[i][1]=l.getStock().get(i).getNum()+"";
 
         }
 
@@ -54,22 +44,23 @@ public class DisplayLists {
 
     }
 
-    public static void DisplayTicketList(TicketList l)
+    public static void DisplayTicketList()
     {
+        String name = JOptionPane.showInputDialog("Input Customer Name");
+
         JFrame frame = new JFrame("JTable Test");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        List<Ticket>ls = l.ticket;
+        List<Ticket>ls = FileTest.tl.ticket.stream().filter(ticket->ticket.getCustomer().getName().equals(name)).collect(Collectors.toList());
         String[] columnNames = {"Ticket Number", "Customer Name", "Brand", "Num"};
 
         Object[][] data = new String[ls.size()][4];
         for(int i=0;i<ls.size();i++)
         {
-            data[i][0]=ls.get(i).getNumber()+"";
-            data[i][1]=ls.get(i).getCustomer().getName();
-            data[i][2]=ls.get(i).getDrink().getBrand();
-            data[i][3]=ls.get(i).getDrink().getNum()+"";
-
+                data[i][0] = ls.get(i).getNumber() + "";
+                data[i][1] = ls.get(i).getCustomer().getName();
+                data[i][2] = ls.get(i).getDrink().getBrand();
+                data[i][3] = ls.get(i).getDrink().getNum() + "";
         }
 
         JTable table = new JTable(data, columnNames);
